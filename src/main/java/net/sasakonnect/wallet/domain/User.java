f96397af35413419fdbb8c71b33e35fe40723673
david macharia <davidkiarie5861@gmail.com>
2023-10-18 11:05:34 +0300

    delete  even pin if the user had set and account was not verified

--- src/main/java/net/sasakonnect/wallet/domain/User.java
+++ src/main/java/net/sasakonnect/wallet/domain/User.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -104,6 +105,8 @@ public class User extends BaseWalletDomain implements Serializable, UserDetails 
 
 	@OneToMany(mappedBy = "user")
 	private List<UserDevice> userDevices;
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<UserPin> userPins;
 
 	public Map<String, Object> toBankPayload() {
 		Map<String, Object> payload = new HashMap<>();
