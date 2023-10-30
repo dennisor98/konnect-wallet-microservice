68fa218a33b2514b697123785b3e9011ed65b8cb
david macharia <davidkiarie5861@gmail.com>
2023-10-30 11:19:47 +0300

    removed recevier account on till

--- src/main/java/net/sasakonnect/wallet/RequestDto/MpesaBilling.java
+++ src/main/java/net/sasakonnect/wallet/RequestDto/MpesaBilling.java
@@ -1,5 +1,6 @@
 package net.sasakonnect.wallet.RequestDto;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -19,7 +20,7 @@ public class MpesaBilling {
 	@NotBlank
 	public String shortCode;
 
-	@NotBlank
+	@Nullable
 	public String receivingAccount;
 
 	@Positive
