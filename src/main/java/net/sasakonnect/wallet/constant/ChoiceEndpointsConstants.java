7d52c95f9df1d2ab1bec83b15e0977b3b5d07822
david macharia <davidkiarie5861@gmail.com>
2023-11-03 16:17:58 +0300

    fixed sms time

--- src/main/java/net/sasakonnect/wallet/constant/ChoiceEndpointsConstants.java
+++ src/main/java/net/sasakonnect/wallet/constant/ChoiceEndpointsConstants.java
@@ -4,8 +4,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ChoiceEndpointsConstants {
-
-	public static final String BASE_URL = "https://baas-pilot.choicebankapi.com";
+///production url https://baas.choicedigitalbank.com
+	// development https://baas-pilot.choicebankapi.com/
+	public static final String BASE_URL = "https://baas.choicedigitalbank.com";
 	public static final String ON_BOARDING = "/onboarding/submitOnboardingRequest";
 	public static final String BANK_CODES = "/staticData/getBankCodes";
 	public static final String UPLOAD_ID_CARDS = "/onboarding/uploadIdCardPhoto";
