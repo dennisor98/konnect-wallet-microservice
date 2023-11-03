8184752a266117da1c94a83dbbb90497063eae96
david macharia <davidkiarie5861@gmail.com>
2023-11-03 21:41:26 +0300

    add args

--- src/main/java/net/sasakonnect/wallet/domain/Role.java
+++ src/main/java/net/sasakonnect/wallet/domain/Role.java
@@ -8,12 +8,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role extends BaseWalletDomain implements Serializable {
 
 	@Column(name = "role_name", unique = true)
