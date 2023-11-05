b124985206eeb728042103b661763ba8bbf02f1a
david macharia <davidkiarie5861@gmail.com>
2023-11-05 16:20:11 +0300

    fixed firebase

--- src/main/java/net/sasakonnect/wallet/beans/BankWebClientBean.java
+++ src/main/java/net/sasakonnect/wallet/beans/BankWebClientBean.java
@@ -1,16 +1,18 @@
 package net.sasakonnect.wallet.beans;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import net.sasakonnect.wallet.constant.ChoiceEndpointsConstants;
-
 @Component
 public class BankWebClientBean {
-	public WebClient webClient = WebClient.builder().baseUrl(ChoiceEndpointsConstants.BASE_URL)
+
+	@Value("${BASE_URL}")
+	private String baseUrl;
+	public WebClient webClient = WebClient.builder().baseUrl(baseUrl)
 			.codecs(configurer -> configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder()))
 
 			.build();
--- src/main/java/net/sasakonnect/wallet/services/RestClientService.java
+++ src/main/java/net/sasakonnect/wallet/services/RestClientService.java
@@ -1,20 +1,22 @@
 package net.sasakonnect.wallet.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import net.sasakonnect.wallet.constant.ChoiceEndpointsConstants;
 import net.sasakonnect.wallet.tools.JwtService;
 
 @Service
 public class RestClientService extends JwtService {
+	@Value("${BASE_URL}")
+	private String baseUrl;
 	protected WebClient webClient;
 	protected Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
 	public RestClientService() {
-		this.webClient = WebClient.builder().baseUrl(ChoiceEndpointsConstants.BASE_URL).build();
+		this.webClient = WebClient.builder().baseUrl(baseUrl).build();
 	}
 
 //	    public Mono<String> fetchData() {
