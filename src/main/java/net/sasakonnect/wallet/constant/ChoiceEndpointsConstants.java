389c223f29a7a58bdb42f240238de0b14cd90514
david macharia <davidkiarie5861@gmail.com>
2023-11-08 17:01:59 +0300

    added wallet service

--- src/main/java/net/sasakonnect/wallet/constant/ChoiceEndpointsConstants.java
+++ src/main/java/net/sasakonnect/wallet/constant/ChoiceEndpointsConstants.java
@@ -23,7 +23,6 @@ public class ChoiceEndpointsConstants {
 	public static final String WITHDRAW = "/trans/applyForTransfer";
 	public static final String BUY_AIRTIME = "/utilityPayment/airtimePayment";
 	public static final String PAY_UTILITY = "/utilityPayment/billPayment";
-
 	public static final String CHECK_BALANCE = "/query/getAccountDetails";
 	public static final String SHORT_CODE = "/account/applyForShortCode";
 	public static final String GET_SHORT_CODE = "/account/queryAccountByShortCode";
