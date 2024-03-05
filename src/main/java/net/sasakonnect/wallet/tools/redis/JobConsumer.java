debb60697e6327b6c1a1db83b44991e8e2f0f6be
david macharia <davidkiarie5861@gmail.com>
2024-03-05 10:58:16 +0300

    separated job queue enviroments

--- src/main/java/net/sasakonnect/wallet/tools/redis/JobConsumer.java
+++ src/main/java/net/sasakonnect/wallet/tools/redis/JobConsumer.java
@@ -1,11 +1,18 @@
 package net.sasakonnect.wallet.tools.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class JobConsumer<T extends Queueable<T>> {
+
+	@Value("${spring.profiles.active:production}:jobQueue")
+	String queueName;
 
 	private final RedisTemplate<String, T> redisTemplate;
 
@@ -16,9 +23,9 @@ public class JobConsumer<T extends Queueable<T>> {
 	@Scheduled(fixedRate = 1000) // Adjust the rate (in milliseconds) as needed
 
 	public void processJobs() {
-		Queueable<T> queue = redisTemplate.opsForList().rightPop("jobQueue");
+		Queueable<T> queue = redisTemplate.opsForList().rightPop(queueName);
 		if (queue != null) {
-			System.out.println("Job exist");
+			log.info("Picking a job with tag " + queueName);
 
 			// Process the job
 			queue.executeJob(queue);
