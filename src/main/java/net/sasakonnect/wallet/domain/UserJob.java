a1207f7e8b98e4df0b188cfb3ab473bcf0379106
Dennis Ronny <dennisor98@gmail.com>
2024-03-20 17:01:18 +0300

    modified statement request notification

--- src/main/java/net/sasakonnect/wallet/domain/UserJob.java
+++ src/main/java/net/sasakonnect/wallet/domain/UserJob.java
@@ -32,7 +32,7 @@ public class UserJob extends BaseWalletDomain implements Serializable {
 	private Boolean isRead;
 	@Column() // Example of a nullable field
 	private Boolean isAdmin;
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "requester_user_id", nullable = true) // Name of the foreign key column in UserJob table
 	private User jobOwner;
 	@Column(nullable = true, columnDefinition = "TEXT")
