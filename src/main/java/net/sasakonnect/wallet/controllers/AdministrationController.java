e1d9b3f6ed9ad217e64fccf51ce5991a96e18a29
Dennis Ronny <dennisor98@gmail.com>
2024-06-11 11:27:37 +0300

    modified financial contact global search

--- src/main/java/net/sasakonnect/wallet/controllers/AdministrationController.java
+++ src/main/java/net/sasakonnect/wallet/controllers/AdministrationController.java
@@ -30,7 +30,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import net.bytebuddy.asm.Advice.This;
 import net.sasakonnect.wallet.RequestDto.Corporate;
 import net.sasakonnect.wallet.RequestDto.NotificationDto;
 import net.sasakonnect.wallet.RequestDto.PermissionDTO;
