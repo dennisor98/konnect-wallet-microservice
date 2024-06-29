fce9402e3f95fca0fb72416b5fbe6be065784d53
david macharia <davidkiarie5861@gmail.com>
2024-06-29 10:25:50 +0300

    separated swagger config

---
+++ src/main/java/net/sasakonnect/wallet/config/SwaggerSecurityConfig.java
@@ -0,0 +1,95 @@
+package net.sasakonnect.wallet.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.headers.Header;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+@EnableWebSecurity
+public class SwaggerSecurityConfig {
+	private final PasswordEncoder passwordEncoder;
+	@Value("${spring.profiles.active}")
+	String profileActive;
+
+	public SwaggerSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails user = User.builder().username("user").password(passwordEncoder.encode("password")).roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	OpenAPI openApiInformation() throws Exception {
+		Server localServer = new Server().url("http://localhost:8081/konnect-wallet")
+				.description("Localhost Server URL");
+		Server gatewayServer = new Server().url("https://gw.sasakonnect.net/konnect-wallet")
+				.description("Gateway Server Server URL(Dev)");
+		Server nginxServer = new Server().url("https://wallet.sasakonnect.net/konnect-wallet")
+				.description("Production env");
+
+		Server ngrokServer = new Server().url("https://328c-105-29-165-232.ngrok-free.app").description("Ngrok env");
+
+		Contact contact = new Contact().email("devops@gmail.com").name("DevOps");
+		Info info = new Info().contact(contact).description("Wallet Based implementation Through Choice Bank")
+				.summary("Easy way to Buy").title("Konnect Wallet").version("V1.0.0")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org"));
+		// Define custom header here
+		Components components = new Components();
+		components.addHeaders("X-Custom-Header",
+				new Header().description("Description of custom header").schema(new StringSchema()));
+		components.addSecuritySchemes("Bearer Authentication", createAPIKeyScheme());
+
+		Object example_token = "xy......bearertoken";
+
+		var openApi = new OpenAPI();
+		openApi.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication")).components(components
+
+		).components(new Components().addSecuritySchemes("basicScheme",
+				new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+				.addSecurityItem(new SecurityRequirement().addList("basicScheme"));
+
+		switch (profileActive) {
+		case "dev": {
+			openApi.info(info).addServersItem(gatewayServer).addServersItem(nginxServer).addServersItem(localServer)
+					.addServersItem(ngrokServer);
+			break;
+
+		}
+		default: {
+			openApi.info(info).addServersItem(nginxServer).addServersItem(gatewayServer).addServersItem(localServer)
+					.addServersItem(ngrokServer);
+			break;
+
+		}
+
+		}
+
+		return openApi;
+	}
+
+	private SecurityScheme createAPIKeyScheme() {
+		return new SecurityScheme().type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("bearer");
+	}
+
+}
--- src/main/java/net/sasakonnect/wallet/config/WebSecurityConfig.java
+++ src/main/java/net/sasakonnect/wallet/config/WebSecurityConfig.java
@@ -36,17 +36,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.headers.Header;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.parameters.HeaderParameter;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import lombok.extern.slf4j.Slf4j;
 import net.sasakonnect.wallet.services.UserService;
 
@@ -156,56 +146,6 @@ public class WebSecurityConfig {
 		var expressionHandler = new DefaultMethodSecurityExpressionHandler();
 		expressionHandler.setPermissionEvaluator(new CustomPermissionEvaluator(userService));
 		return expressionHandler;
-	}
-
-	@Bean
-	OpenAPI openApiInformation() throws Exception {
-		Server localServer = new Server().url("http://localhost:8081/konnect-wallet")
-				.description("Localhost Server URL");
-		Server gatewayServer = new Server().url("https://gw.sasakonnect.net/konnect-wallet")
-				.description("Gateway Server Server URL(Dev)");
-		Server nginxServer = new Server().url("https://wallet.sasakonnect.net/konnect-wallet")
-				.description("Production env");
-
-		Server ngrokServer = new Server().url("https://328c-105-29-165-232.ngrok-free.app").description("Ngrok env");
-
-		Contact contact = new Contact().email("devops@gmail.com").name("DevOps");
-		Info info = new Info().contact(contact).description("Wallet Based implementation Through Choice Bank")
-				.summary("Easy way to Buy").title("Konnect Wallet").version("V1.0.0")
-				.license(new License().name("Apache 2.0").url("http://springdoc.org"));
-		// Define custom header here
-		Components components = new Components();
-		components.addHeaders("X-Custom-Header",
-				new Header().description("Description of custom header").schema(new StringSchema()));
-		components.addSecuritySchemes("Bearer Authentication", createAPIKeyScheme());
-
-		Object example_token = "xy......bearertoken";
-		var openApi = new OpenAPI();
-		openApi.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication")).components(components
-
-		);
-
-		switch (profileActive) {
-		case "dev": {
-			openApi.info(info).addServersItem(gatewayServer).addServersItem(nginxServer).addServersItem(localServer)
-					.addServersItem(ngrokServer);
-			break;
-
-		}
-		default: {
-			openApi.info(info).addServersItem(nginxServer).addServersItem(gatewayServer).addServersItem(localServer)
-					.addServersItem(ngrokServer);
-			break;
-
-		}
-
-		}
-
-		return openApi;
-	}
-
-	private SecurityScheme createAPIKeyScheme() {
-		return new SecurityScheme().type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("bearer");
 	}
 
 	@Bean
