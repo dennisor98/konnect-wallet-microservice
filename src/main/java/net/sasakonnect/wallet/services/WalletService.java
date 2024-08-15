70ff50313b8a01219dbebb920a8f63fa46570d29
Dennis Ronny <dennisor98@gmail.com>
2024-08-15 13:04:05 +0300

    rectified account upgrade check bug

--- src/main/java/net/sasakonnect/wallet/services/WalletService.java
+++ src/main/java/net/sasakonnect/wallet/services/WalletService.java
@@ -270,7 +270,7 @@ public class WalletService {
 		    				dataMap.put("canSkip",canSkip);
 		    				dataMap.put("onboardingStatus", onboardingStatus);
 		    				dataMap.put("title", "Account Upgrade Required");
-		    				dataMap.put("accountType", "C002");
+		    				dataMap.put("accountType",accountType);
 		    				map.put("data", dataMap);
 		    				return map;
 		    			}
