648cd25fecd2e7cf0509a2843823db2b200b0f3c
Dennis Ronny <dennisor98@gmail.com>
2024-09-02 14:41:25 +0300

    added rejected account search api

--- src/main/java/net/sasakonnect/wallet/repository/RejectedAccountRepository.java
+++ src/main/java/net/sasakonnect/wallet/repository/RejectedAccountRepository.java
@@ -2,11 +2,14 @@ package net.sasakonnect.wallet.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import net.sasakonnect.wallet.domain.RejectedAccount;
+import net.sasakonnect.wallet.domain.User;
 
 public interface RejectedAccountRepository extends JpaRepository<RejectedAccount,String> {
    void deleteByIdNumber(String idNumber);
@@ -15,4 +18,8 @@ public interface RejectedAccountRepository extends JpaRepository<RejectedAccount
    Optional<RejectedAccount> findByMobile(@Param("mobile") String mobile);
    
    Optional<RejectedAccount> findByIdNumber(String idNumber);
+   
+   @Query("SELECT u FROM RejectedAccount u   WHERE u.firstName LIKE %:queryString% "
+			+ "OR u.lastName LIKE %:queryString% OR u.middleName LIKE %:queryString% OR u.idNumber LIKE  %:queryString% OR u.mobile LIKE %:queryString%")
+	Page<RejectedAccount> searchUser(@Param("queryString") String queryString, Pageable pageable);
 }
