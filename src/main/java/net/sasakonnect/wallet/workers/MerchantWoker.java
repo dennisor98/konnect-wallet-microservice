5ccdb3e59fc5717c0d3e8b46707b0a83fb8310f8
Dennis Ronny <dennisor98@gmail.com>
2024-10-31 01:38:55 +0300

    modified merchant tx notifier

--- src/main/java/net/sasakonnect/wallet/workers/MerchantWoker.java
+++ src/main/java/net/sasakonnect/wallet/workers/MerchantWoker.java
@@ -38,7 +38,7 @@ public class MerchantWoker {
 	@Transactional
 	public void notifyMerchant(Transaction transaction) {
 		if (transaction.getTxType().equalsIgnoreCase("TTID0005") || (transaction.getTxType().equalsIgnoreCase(NotificationType.TRANSACTION.getCode()) && 
-				transaction.getOppoAccountId().equalsIgnoreCase("46011000000433")) ) {
+				transaction.getOppoAccountId().equalsIgnoreCase("46012000048133")) ) {
 			Transaction newTransaction = Transaction.builder().txId(transaction.getTxId())
 
 					.externalTxId(transaction.getExternalTxId()).accountId("***********")
